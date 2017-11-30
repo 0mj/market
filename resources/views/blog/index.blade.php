fb61f8dd2bb775d8c77d3fcb037b312142e9f124
Fran Zappernick <mmjamiso@kent.edu>
2017-11-30 15:41:11 -0500

    FZ

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -4,6 +4,7 @@
 
     <div class="row">
         <div class="col">
+            <h1>Fran Zappernick</h1>
             <h1>this renders from blog/index.blade.php </h1>
             <p>{{"which is configured within C:\Users\Matthew.Jamison\project_FormToTable\market\routes\web.php "}}</p>
         </div>
